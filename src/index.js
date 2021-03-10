c23601cebef8bd001cf8251832e12e835ab39873
Arcadeena <mattmcc97@gmail.com>
2021-03-10 20:15:53 +1100

    Add voice state update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ module.exports = (client) => {
         user: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        connectionLimit: 10
+        connectionLimit: 10,
+        supportBigInt: true
     });
     pool.getConnection().then(conn => {
         console.log('Connected to database', process.env.DB_NAME);
@@ -16,51 +17,55 @@ module.exports = (client) => {
         const messageDelete = require('./archivers/messageDelete');
         const messageReaction = require('./archivers/messageReaction');
         const presenceUpdate = require('./archivers/presenceUpdate');
+        const voiceStateUpdate = require('./archivers/voiceStateUpdate');
 
         const totals = {};
         client.on('raw', packet => {
-            if (packet.t) {
-                if (Object.keys(totals).length) {
-                    for (let i = 0; i < Object.keys(totals).length + 3; i++) {
-                        process.stdout.moveCursor(0, -1) // up one line
-                        process.stdout.clearLine(1) // from cursor to end
-                    }
-                }
-
-                if (!totals[packet.t]) totals[packet.t] = 0;
-                totals[packet.t]++;
-
-                console.log();
-                console.log('----------Totals----------');
-                Object.keys(totals).forEach(key => {
-                    if (key === packet.t) console.log(key, totals[key], '(latest)')
-                    else console.log(key, totals[key])
-                })
-                console.log('--------------------------');
-            }
+            // if (packet.t) {
+            //     if (Object.keys(totals).length) {
+            //         for (let i = 0; i < Object.keys(totals).length + 3; i++) {
+            //             process.stdout.moveCursor(0, -1) // up one line
+            //             process.stdout.clearLine(1) // from cursor to end
+            //         }
+            //     }
+            //
+            //     if (!totals[packet.t]) totals[packet.t] = 0;
+            //     totals[packet.t]++;
+            //
+            //     console.log();
+            //     console.log('----------Totals----------');
+            //     Object.keys(totals).forEach(key => {
+            //         if (key === packet.t) console.log(key, totals[key], '(latest)')
+            //         else console.log(key, totals[key])
+            //     })
+            //     console.log('--------------------------');
+            // }
 
             // if (packet.t) console.log('[', new Date(), ']', 'Event', counter++, packet.t);
             switch (packet.t) {
 
-                case 'MESSAGE_CREATE':
-                    return messageCreate(client, packet, pool);
-                case 'MESSAGE_UPDATE':
-                    return messageUpdate(client, packet, pool);
-                case 'MESSAGE_DELETE':
-                case 'MESSAGE_DELETE_BULK':
-                    return messageDelete(client, packet, pool);
+                // case 'MESSAGE_CREATE':
+                //     return messageCreate(client, packet, pool);
+                // case 'MESSAGE_UPDATE':
+                //     return messageUpdate(client, packet, pool);
+                // case 'MESSAGE_DELETE':
+                // case 'MESSAGE_DELETE_BULK':
+                //     return messageDelete(client, packet, pool);
+                //
+                // case 'MESSAGE_REACTION_ADD':
+                //     return messageReaction.add(client, packet, pool);
+                // case 'MESSAGE_REACTION_REMOVE':
+                //     return messageReaction.remove(client, packet, pool);
+                // case 'MESSAGE_REACTION_REMOVE_ALL':
+                //     return messageReaction.removeAll(client, packet, pool);
+                // case 'MESSAGE_REACTION_REMOVE_EMOJI':
+                //     return messageReaction.removeEmoji(client, packet, pool);
+                //
+                // case 'PRESENCE_UPDATE':
+                //     return presenceUpdate(client, packet, pool);
 
-                case 'MESSAGE_REACTION_ADD':
-                    return messageReaction.add(client, packet, pool);
-                case 'MESSAGE_REACTION_REMOVE':
-                    return messageReaction.remove(client, packet, pool);
-                case 'MESSAGE_REACTION_REMOVE_ALL':
-                    return messageReaction.removeAll(client, packet, pool);
-                case 'MESSAGE_REACTION_REMOVE_EMOJI':
-                    return messageReaction.removeEmoji(client, packet, pool);
-
-                case 'PRESENCE_UPDATE':
-                    return presenceUpdate(client, packet, pool);
+                case 'VOICE_STATE_UPDATE':
+                    return voiceStateUpdate(client, packet, pool);
 
                 case 'CHANNEL_PINS_UPDATE': // This causes a message update too
                 default:
---
+++ src/sql/voice_state/voice_state.sql
@@ -0,0 +1,17 @@
+-- https://discord.com/developers/docs/resources/voice#voice-state-object
+CREATE TABLE voice_state (
+    epoch BIGINT NOT NULL,
+    guild_id BIGINT,
+    channel_id BIGINT,
+    user_id BIGINT NOT NULL,
+    -- [ommited] member
+    session_id VARCHAR(256) NOT NULL,
+    deaf BOOLEAN NOT NULL,
+    mute BOOLEAN NOT NULL,
+    self_deaf BOOLEAN NOT NULL,
+    self_mute BOOLEAN NOT NULL,
+    self_stream BOOLEAN NOT NULL,
+    self_video BOOLEAN NOT NULL,
+    -- [ommited] supress
+    CONSTRAINT id PRIMARY KEY (epoch)
+);
