1c33cbc1b95677a7d6b81de5ec4aed35148dee49
Arcadeena <mattmcc97@gmail.com>
2021-03-10 23:52:06 +1100

    Pool limit increase

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ module.exports = (client) => {
         user: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        connectionLimit: 10,
+        connectionLimit: 100,
         supportBigInt: true
     });
     pool.getConnection().then(conn => {
