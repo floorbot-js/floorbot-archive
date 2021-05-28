04c4372467e137ab0f62bb44c70e498c631d1bc1
Arcadeena <mattmcc97@gmail.com>
2021-05-28 23:19:17 +0930

    Fixes

--- src/app.js
+++ src/app.js
@@ -10,17 +10,24 @@ const pool = mariadb.createPool({
     supportBigInt: true
 });
 
-const { Client, Intents } = require('discord.js-commands')(require('discord.js'));
-const ArchiveClient = require('./index')(Client, pool);
+const testDB = function(client) {
+    return pool.query().then(res => client.emit('log', `[SETUP] Connected to database <${process.env.DB_NAME}>`))
+        .catch(error => client.emit('log', `[SETUP] Failed to connect to database <${process.env.DB_NAME}>`, error))
+}
 
-const client = new ArchiveClient({
+const { Client, Intents } = require('discord.js-commands')(require('discord.js'));
+const archive = require('./index')(pool);
+const client = new Client({
     token: process.env.DISCORD_TOKEN,
     publicKey: process.env.DISCORD_PUBLIC_KEY,
-    intents: Intents.ALL
+    intents: Intents.ALL,
+    tasks: archive.tasks
 });
 
 client.on('log', (string, object) => { console.log(string) })
 client.on('ready', () => client.emit('log', `[SETUP] Logged in as <${client.user.tag}>`));
-pool.query().then(res => client.emit('log', `[SETUP] Connected to database <${process.env.DB_NAME}>`))
-    .catch(error => client.emit('log', `[SETUP] Failed to connect to database <${process.env.DB_NAME}>`, error))
-    .then(() => client.login())
+(async () => {
+    await testDB(client);
+    await archive.setup(client);
+    await client.login();
+})()
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
-module.exports = (CommandClient, pool) => class extends CommandClient {
-    constructor(options) {
-        options.tasks = Object.assign({
+module.exports = (pool) => {
+    return {
+        tasks: {
             archiveStats: { class: require('./archive/tasks/ArchiveStats'), options: { pool } },
 
             archiveGuildMember: { class: require('./archive/tasks/ArchiveGuildMember'), options: { pool } },
@@ -12,68 +12,63 @@ module.exports = (CommandClient, pool) => class extends CommandClient {
             archivePresence: { class: require('./archive/tasks/ArchivePresence'), options: { pool } },
             archiveTyping: { class: require('./archive/tasks/ArchiveTyping'), options: { pool } },
             archiveVoiceState: { class: require('./archive/tasks/ArchiveVoiceState'), options: { pool } }
-        }, options.tasks);
-        super(options);
-    }
+        },
+        setup: (client) => {
+            return Promise.allSettled([
+                pool.query(fs.readFileSync(`${__dirname}/../res/sql/guild_member.sql`, 'utf8')),
+                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message_delete.sql`, 'utf8')),
+                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message_reaction.sql`, 'utf8')),
+                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message.sql`, 'utf8')),
+                pool.query(fs.readFileSync(`${__dirname}/../res/sql/presence.sql`, 'utf8')),
+                pool.query(fs.readFileSync(`${__dirname}/../res/sql/typing_start.sql`, 'utf8')),
+                pool.query(fs.readFileSync(`${__dirname}/../res/sql/voice_state.sql`, 'utf8'))
+            ]).then(results => {
+                const error = results.find(result => result.reason && result.reason.code && result.reason.code !== 'ER_TABLE_EXISTS_ERROR');
+                if (error) return Promise.reject(error);
+                else return Promise.allSettled([
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/guild_member/guild_member_role.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/guild_member/guild_member_user.sql`, 'utf8')),
 
-    login(token) {
-        return Promise.allSettled([
-            pool.query(fs.readFileSync(`${__dirname}/../res/sql/guild_member.sql`, 'utf8')),
-            pool.query(fs.readFileSync(`${__dirname}/../res/sql/message_delete.sql`, 'utf8')),
-            pool.query(fs.readFileSync(`${__dirname}/../res/sql/message_reaction.sql`, 'utf8')),
-            pool.query(fs.readFileSync(`${__dirname}/../res/sql/message.sql`, 'utf8')),
-            pool.query(fs.readFileSync(`${__dirname}/../res/sql/presence.sql`, 'utf8')),
-            pool.query(fs.readFileSync(`${__dirname}/../res/sql/typing_start.sql`, 'utf8')),
-            pool.query(fs.readFileSync(`${__dirname}/../res/sql/voice_state.sql`, 'utf8'))
-        ]).then(results => {
-            const error = results.find(result => result.reason && result.reason.code && result.reason.code !== 'ER_TABLE_EXISTS_ERROR');
-            if (error) return Promise.reject(error);
-            else return Promise.allSettled([
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/guild_member/guild_member_role.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/guild_member/guild_member_user.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_activity.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_application.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_attachment.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_author.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_embed.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_mention_channel.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_mention_role.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_mention_special.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_mention_user.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_reference.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_sticker.sql`, 'utf8')),
 
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_activity.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_application.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_attachment.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_author.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_embed.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_mention_channel.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_mention_role.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_mention_special.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_mention_user.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_reference.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/message_sticker.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/presence/presence_activity.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/presence/presence_client_status.sql`, 'utf8')),
+                ])
+            }).then(results => {
+                const error = results.find(result => result.reason && result.reason.code && result.reason.code !== 'ER_TABLE_EXISTS_ERROR');
+                if (error) return Promise.reject(error);
+                else return Promise.allSettled([
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/embed/message_embed_author.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/embed/message_embed_field.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/embed/message_embed_footer.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/embed/message_embed_image.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/embed/message_embed_provider.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/embed/message_embed_thumbnail.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/embed/message_embed_video.sql`, 'utf8')),
 
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/presence/presence_activity.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/presence/presence_client_status.sql`, 'utf8')),
-            ])
-        }).then(results => {
-            const error = results.find(result => result.reason && result.reason.code && result.reason.code !== 'ER_TABLE_EXISTS_ERROR');
-            if (error) return Promise.reject(error);
-            else return Promise.allSettled([
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/embed/message_embed_author.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/embed/message_embed_field.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/embed/message_embed_footer.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/embed/message_embed_image.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/embed/message_embed_provider.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/embed/message_embed_thumbnail.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/message/embed/message_embed_video.sql`, 'utf8')),
-
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/presence/activity/presence_activity_assets.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/presence/activity/presence_activity_emoji.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/presence/activity/presence_activity_party.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/presence/activity/presence_activity_secrets.sql`, 'utf8')),
-                pool.query(fs.readFileSync(`${__dirname}/../res/sql/presence/activity/presence_activity_timestamps.sql`, 'utf8'))
-            ])
-        }).then(results => {
-            const error = results.find(result => result.reason && result.reason.code && result.reason.code !== 'ER_TABLE_EXISTS_ERROR');
-            if (!error) return this.emit('log', '[SETUP](Archive) database setup successful');
-            else return Promise.reject(error);
-        }).catch(error => {
-            console.log(error)
-            this.emit('log', '[SETUP](Archive) database setup failed', error)
-        }).then(() => {
-            return super.login(token);
-        })
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/presence/activity/presence_activity_assets.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/presence/activity/presence_activity_emoji.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/presence/activity/presence_activity_party.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/presence/activity/presence_activity_secrets.sql`, 'utf8')),
+                    pool.query(fs.readFileSync(`${__dirname}/../res/sql/presence/activity/presence_activity_timestamps.sql`, 'utf8'))
+                ])
+            }).then(results => {
+                const error = results.find(result => result.reason && result.reason.code && result.reason.code !== 'ER_TABLE_EXISTS_ERROR');
+                if (!error) return client.emit('log', '[SETUP](Archive) database setup successful');
+                else return Promise.reject(error);
+            }).catch(error => {
+                client.emit('log', '[SETUP](Archive) database setup failed', error)
+            })
+        }
     }
 }
